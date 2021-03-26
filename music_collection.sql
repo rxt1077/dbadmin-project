fea909908d997ef7269369f452df65ed58aef406
JazmineSA508 <jazminesarrington@gmail.com>
2021-03-26 12:52:52 -0400

    added FINNEAS discography to SQL file

--- music_collection.sql
+++ music_collection.sql
@@ -7,7 +7,7 @@ CREATE DATABASE music_collection;
 
 DROP TABLE IF EXISTS artists;
 CREATE TABLE artists (
-  name varchar(100) NOT NULL UNIQUE,
+  name varchar(100) NOT NULL,
   genre varchar(100) NOT NULL,
   PRIMARY KEY (name)
 );
@@ -38,13 +38,13 @@ INSERT INTO valid_media_type (mtype) VALUES
 
 DROP TABLE IF EXISTS music_catalog;
 CREATE TABLE music_catalog (
-  id serial NOT NULL,
+  id serial NOT NULL UNIQUE,
   artist varchar(100) NOT NULL,
   media_type text REFERENCES valid_media_type (mtype),
   song_title varchar(100) NOT NULL DEFAULT '',
   album_title varchar(100) NOT NULL DEFAULT '',
   release_year varchar(4) DEFAULT NULL,
-  PRIMARY KEY (artist, song_title),
+  PRIMARY KEY (id, artist, song_title),
   CONSTRAINT music_cat_idx FOREIGN KEY (artist) REFERENCES artists (name) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -86,5 +86,29 @@ INSERT INTO music_catalog (artist, media_type, song_title, album_title, release_
   ('Billie Eilish', 'song', 'Therefore I Am', '', 2020),
   ('Billie Eilish', 'song', 'Lo Vas A Olvidar', '', 2021),
   ('FINNEAS', 'song', 'New Girl', '', 2016),
-  ('FINNEAS', 'song', 'I''m in Love Without You', '', 2017)
+  ('FINNEAS', 'song', 'I''m in Love Without You', '', 2017),
+  ('FINNEAS', 'song', 'Heaven', '', 2018),
+  ('FINNEAS', 'song', 'Life Moves On', '', 2018),
+  ('FINNEAS', 'song', 'Landmine', '', 2018),
+  ('FINNEAS', 'song', 'Hollywood Forever', '', 2018),
+  ('FINNEAS', 'song', 'College', '', 2018),
+  ('FINNEAS', 'song', 'Luck Pusher', '', 2018),
+  ('FINNEAS', 'song', 'Claudia', '', 2019),
+  ('FINNEAS', 'song', 'Angel', '', 2019),
+  ('FINNEAS', 'ep', '', 'Blood Harmony', 2019),
+  ('FINNEAS', 'song', 'I Lost a Friend', 'Blood Harmony', 2019),
+  ('FINNEAS', 'song', 'Shelter', 'Blood Harmony', 2019),
+  ('FINNEAS', 'song', 'Lost My Mind', 'Blood Harmony', 2019),
+  ('FINNEAS', 'song', 'I Don''t Miss You at All', 'Blood Harmony', 2019),
+  ('FINNEAS', 'song', 'Partners in Crime', 'Blood Harmony', 2019),
+  ('FINNEAS', 'song', 'Let''s Fall in Love for the Night', 'Blood Harmony', 2019),
+  ('FINNEAS', 'song', 'Die Alone', 'Blood Harmony', 2019),
+  ('FINNEAS', 'album', '', 'Blood Harmony (Deluxe)', 2020),
+  ('FINNEAS', 'song', 'Break My Heart Again', 'Blood Harmony (Deluxe)', 2020),
+  ('FINNEAS', 'song', 'Let''s Fall in Love for the Night (1964)', 'Blood Harmony (Deluxe)', 2020),
+  ('FINNEAS', 'song', 'Can''t Wait To Be Dead', '', 2020),
+  ('FINNEAS', 'song', 'Where the Poison Is', '', 2020),
+  ('FINNEAS', 'song', 'Another Year', '', 2020),
+  ('FINNEAS', 'song', 'American Cliché', '', 2021),
+  ('FINNEAS', 'song', 'Till Forever Falls Apart', '', 2021),
 ;
